5d93c4d48e8b09d46e92be6bca6a253ac4474366
nico <nicorobledo92@hotmail.com>
2021-12-01 18:52:43 -0300

    algo nuevo

--- src/db.js
+++ src/db.js
@@ -3,9 +3,9 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {DB_USER, DB_PASSWORD, DB_HOST} = process.env;
-
+/* postgres://kvhkwuag:***@motty.db.elephantsql.com/kvhkwuag  */
 /* const sequelize = new Sequelize(`postgres://postgres:15Trucho@localhost:5432/videogames` */
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/Videogames`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_USER}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
